437a1ae50b4eae5830445f3b94fca8179d61553f
Henauxg <19689618+Henauxg@users.noreply.github.com>
2024-07-29 12:53:09 +0200

    Enable state scoped entities for `ArenaMode`

--- src/screen/mod.rs
+++ src/screen/mod.rs
@@ -4,9 +4,12 @@ mod playing;
 
 use bevy::prelude::*;
 
+use crate::game::arena::ArenaMode;
+
 pub(super) fn plugin(app: &mut App) {
     app.init_state::<Screen>();
     app.enable_state_scoped_entities::<Screen>();
+    app.enable_state_scoped_entities::<ArenaMode>();
 
     app.add_plugins((loading::plugin, main_menu::plugin, playing::plugin));
 }
